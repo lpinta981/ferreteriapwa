d0e7f7b7f6747043b346e194625cea6dee9d8c15
lpinta981 <108894335+lpinta981@users.noreply.github.com>
2024-08-22 13:24:52 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -3,11 +3,12 @@ document.getElementById('login-form').addEventListener('submit', async function(
     const usuario = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyhQI-T-F-NBEqIQQCoMBQDUMw_rlPKRn-1PNO68vjjtTNUv_GNssMYLhDmLYA4mBHJ/exec', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ usuario, password }),
-    });
+const response = await fetch('https://script.google.com/macros/s/AKfycbyhQI-T-F-NBEqIQQCoMBQDUMw_rlPKRn-1PNO68vjjtTNUv_GNssMYLhDmLYA4mBHJ/exec', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ usuario, password }),
+    mode: 'no-cors' // Agregar esta línea
+});
 
     const result = await response.json();
     if (result.autorizado) {
